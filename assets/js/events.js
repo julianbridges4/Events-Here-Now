9a3d00c755c4b775fd5a69021a27603048883387
Julian K. Bridges <julianbridges01@gmail.com>
2017-08-10 20:12:33 -0400

    Added autocomplete to search box

--- assets/js/events.js
+++ assets/js/events.js
@@ -26,7 +26,7 @@ function initMap() {
         zoom: 15
     });
     infoWindow = new google.maps.InfoWindow;
-
+    initAutocomplete();
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -55,6 +55,24 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
 }
+
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */(document.getElementById('location')),
+        {types: ['geocode']});
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+  }
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+
+  }
 
 //uses a json p callback ; 
 
